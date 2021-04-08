fca8f8c44d54e88be51905bfd61c9cc23360cfd9
Dev <dev@lotech.info>
2021-04-07 23:18:41 -0700

    Add stack.

--- src/Stack.ts
+++ src/Stack.ts
@@ -1,1 +1,25 @@
-export default class Stack {}
+import Node from "./Node";
+
+export default class Stack {
+    private items: Node[] = [];
+
+    isEmpty() {
+        return this.items.length == 0;
+    }
+
+    push(node: Node) {
+        this.items.push(node);
+    }
+
+    count() {
+        return this.items.length;
+    }
+
+    pop() {
+        return this.items.pop();
+    }
+
+    peek() {
+        return this.items[this.count() -1];
+    }
+}
