6d3dee5407b1ef33aeb89007a29fcd447c1db7d9
Dev <dev@lotech.info>
2021-04-07 23:43:35 -0700

    Add queue

--- src/Queue.ts
+++ src/Queue.ts
@@ -1,1 +1,29 @@
-export default class Queue {}
+import Node from "./Node";
+
+export default class Queue {
+    private items: Node[] = [];
+
+    isEmpty() {
+        return this.items.length == 0;
+    }
+
+    enqueue(node: Node) {
+        this.items.push(node);
+    }
+
+    dequeue() {
+        return this.items.shift();
+    }
+
+    length() {
+        return this.items.length;
+    }
+
+    front() {
+        return this.items[0];
+    }
+
+    rear() {
+        return this.items[this.length() -1];
+    }
+}
